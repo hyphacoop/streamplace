edfc2067b2a821101a866d2d0e8c486c21e8fadd
Natalie B. <22222885+espeon@users.noreply.github.com>
2025-06-04 15:34:13 -0500

    Update embed.tsx

--- js/app/src/screens/embed.tsx
+++ js/app/src/screens/embed.tsx
@@ -1,11 +1,25 @@
 import { Player } from "components";
 import { PlayerProps } from "components/player/props";
+import {
+  setSidebarHidden,
+  setSidebarUnhidden,
+} from "features/base/sidebarSlice";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { isWeb } from "tamagui";
 import { queryToProps } from "./util";
 
 export default function EmbedScreen({ route }) {
   const { user, protocol, url } = route.params;
   let extraProps: Partial<PlayerProps> = {};
+  let dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(setSidebarHidden());
+    () => {
+      // on unmount, unhide the sidebar
+      dispatch(setSidebarUnhidden());
+    };
+  }, []);
   if (isWeb) {
     extraProps = queryToProps(new URLSearchParams(window.location.search));
   }
