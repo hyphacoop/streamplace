7ed2061e6ca56ec1dd1ff818828f837035924861
Eli Mallon <eli@stream.place>
2025-06-06 19:07:29 -0700

    build: build darwin packages on linux (!) (#256)
    * ci: add osxcross
    
    * ci: build-mac
    
    * ci: working darwin-amd64 build
    
    * ci: dockerfile build stage
    
    * build: use clang from llvm repos
    
    * ci: builder-rebuild is a tiny job
    
    * ci: skip desktop for now
    
    * ci: add darwin codesigning
    
    * ci: cargo install apple-codesign
    
    * ci: cstreamplace for everyone but darwin
    
    * this commit left blank
    
    * ci: recombine images now that it's fast
    
    * ci: arch typo
    
    * ci: add notarization
    
    * ci: gradle no daemon
    
    * ci: zip it uppppp
    
    * empty commit for build
    
    * one last empty commit for build
    
    * github ci: clean disk a bit before building
    
    * ci: re-add darwin desktop build and ios build
    
    * github: simpler approach for saving build space
    
    * gitlab: handle command with spaces
    
    * Makefile: typo
    
    * Makefile: semi colons
    
    * unlock keychain for signing
    
    * build tweaks
    
    * nothing to be done lmao
    
    * ci: we should really build darwin-amd64 i guess
    
    * asodfij
    
    * codesign codemine

--- util/mac-codesign.sh
+++ util/mac-codesign.sh
@@ -21,15 +21,15 @@ function livepeer-keychain() {
   if [[ "${KEYCHAIN_FILE:-}" == "" ]]; then
     KEYCHAIN_FILE="$(security default-keychain | sed -e 's:^["\t ]*::;s:["\t ]*$::')"
   fi
-}
-
-function livepeer-codesign() {
   echo "${DEVELOPER_CERTIFICATE_BASE64}" | base64 -d >"$CERTIFICATE_FILE"
   security unlock-keychain -p "$KEYCHAIN_PASSWORD" "$KEYCHAIN_NAME"
   security import "${CERTIFICATE_FILE}" -f pkcs12 -k "$KEYCHAIN_NAME" -T "$CODESIGN" -P "${DEVELOPER_CERTIFICATE_PASSWORD}"
   security set-key-partition-list -S "apple-tool:,apple:,codesign:" -s -k "$KEYCHAIN_PASSWORD" "$KEYCHAIN_NAME"
-  $CODESIGN --force --sign "${DEVELOPER_CERTIFICATE_ID}" -o runtime "${BINARY_PATH}"
   rm -f "${CERTIFICATE_FILE}"
+}
+
+function livepeer-codesign() {
+  $CODESIGN --force --sign "${DEVELOPER_CERTIFICATE_ID}" -o runtime "${BINARY_PATH}"
   zip -9r "${NOTARIZATION_FILE}" "${BINARY_PATH}"
 }
 
@@ -57,12 +57,10 @@ function livepeer-notarize() {
   rm -f "${NOTARIZATION_FILE}"
 }
 
-function main() {
-  livepeer-keychain
+export BINARY_PATH="${1:-}"
+livepeer-keychain
+if [[ "${BINARY_PATH:-}" != "" ]]; then
   livepeer-codesign
   livepeer-notarize
-}
-
-export BINARY_PATH="$1"
-main
-codesign -dvv "$BINARY_PATH"
+  codesign -dvv "$BINARY_PATH"
+fi
