aa8e67217d0635a320c808a3c7cceda2600415b7
Natalie B. <22222885+espeon@users.noreply.github.com>
2025-06-08 18:56:18 -0500

    Revert "double check in hook"
    This reverts commit b84e7c7258e14dcbd5007c284017818391271cc0.

--- js/app/hooks/useSidebarControl.tsx
+++ js/app/hooks/useSidebarControl.tsx
@@ -58,7 +58,6 @@ export function useSidebarControl(): UseSidebarOutput {
   const isActive = useIsLargeScreen();
   useEffect(() => {
     if (isActive) {
-      if (!isHidden && targetWidth < 64) targetWidth == 64;
       // Only animate if the sidebar is active
       animatedWidth.value = withTiming(targetWidth, { duration: 250 });
     } else {
