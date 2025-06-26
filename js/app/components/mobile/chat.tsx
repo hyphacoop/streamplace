bc84957b18e13aa975a4b195d01bf0fe288e6fab
Natalie B. <22222885+espeon@users.noreply.github.com>
2025-06-26 15:02:45 -0500

    Simplify chat layout a tad

--- js/app/components/mobile/chat.tsx
+++ js/app/components/mobile/chat.tsx
@@ -1,19 +1,15 @@
-import { atoms, Chat, View } from "@streamplace/components";
-import { flex, px } from "@streamplace/components/src/lib/theme/atoms";
-import ChatBox from "components/chat/chat-box";
+import { atoms, Chat, ChatBox, View } from "@streamplace/components";
+import { flex, px, py } from "@streamplace/components/src/lib/theme/atoms";
 const { borderRadius, gap, layout } = atoms;
 
 export function ChatPanel() {
   return (
     <>
-      <View style={[flex.shrink[1], px[1], { marginBottom: -6 }]}>
+      <View style={[flex.shrink[1], px[4]]}>
         <Chat />
       </View>
-      <View style={[layout.flex.column, gap.all[2], px[4]]}>
-        <ChatBox
-          isChatVisible={true}
-          chatBoxStyle={{ borderRadius: borderRadius.xl }}
-        />
+      <View style={[layout.flex.column, gap.all[2], px[4], py[2]]}>
+        <ChatBox chatBoxStyle={{ borderRadius: borderRadius.xl }} />
       </View>
     </>
   );
