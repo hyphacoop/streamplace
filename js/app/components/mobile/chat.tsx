408cd8fc2d4b25f9eef89b90c0037927156941f7
Natalie B. <22222885+espeon@users.noreply.github.com>
2025-06-27 02:18:15 -0500

    Rework chat layout
    - Chat is now absolute when needed - Set max width to 100% - TextArea is
    now flexible and not set at 100%

--- js/app/components/mobile/chat.tsx
+++ js/app/components/mobile/chat.tsx
@@ -4,13 +4,17 @@ const { borderRadius, gap, layout } = atoms;
 
 export function ChatPanel() {
   return (
-    <>
-      <View style={[flex.shrink[1], px[4]]}>
-        <Chat />
-      </View>
+    <View
+      style={[
+        layout.flex.column,
+        flex.shrink[1],
+        { width: "100%", maxWidth: "100%" },
+      ]}
+    >
+      <Chat />
       <View style={[layout.flex.column, gap.all[2], px[4], py[2]]}>
         <ChatBox chatBoxStyle={{ borderRadius: borderRadius.xl }} />
       </View>
-    </>
+    </View>
   );
 }
