a0dc609bd173f4db22f7b10c04aa54b4d8f78a6a
Eli Mallon <eli@aquareum.tv>
2025-07-31 16:42:52 -0700

    ci: on github, run e2e tests in series

--- js/desktop/src/tests/test-runner.ts
+++ js/desktop/src/tests/test-runner.ts
@@ -21,6 +21,8 @@ const allTests: Record<string, E2ETest> = {
 
 export const allTestNames = Object.keys(allTests);
 
+const series = process.env.STREAMPLACE_TEST_SERIES === "true";
+
 export default async function runTests(
   tests: string[],
   duration: string,
@@ -35,66 +37,74 @@ export default async function runTests(
   }
   try {
     const results: string[] = [];
-    const proms: Promise<void>[] = [];
+    const funcs: (() => Promise<void>)[] = [];
     for (const testName of testsToRun) {
       const test = allTests[testName];
       console.log(`============ running test ${testName} ============`);
       let testProc: ChildProcess | undefined;
-      const prom = (async (test) => {
-        try {
-          const { skipNode } = getEnv();
-          const hexKey = privateKey.slice(2); // Remove 0x prefix
-          const exportedKey = new Uint8Array(
-            hexKey.match(/.{1,2}/g).map((byte) => parseInt(byte, 16)),
-          );
-          const multibaseKey = bytesToMultibase(exportedKey, "base58btc");
-          const account = privateKeyToAccount(privateKey);
-          const tmpDir = await fs.mkdtemp(
-            path.join(os.tmpdir(), "streamplace-test-"),
-          );
+      const testFunc = ((test) => {
+        return async () => {
+          try {
+            const { skipNode } = getEnv();
+            const hexKey = privateKey.slice(2); // Remove 0x prefix
+            const exportedKey = new Uint8Array(
+              hexKey.match(/.{1,2}/g).map((byte) => parseInt(byte, 16)),
+            );
+            const multibaseKey = bytesToMultibase(exportedKey, "base58btc");
+            const account = privateKeyToAccount(privateKey);
+            const tmpDir = await fs.mkdtemp(
+              path.join(os.tmpdir(), "streamplace-test-"),
+            );
 
-          let testEnv: TestEnv = {
-            addr: "http://127.0.0.1:38080",
-            internalAddr: "http://127.0.0.1:39090",
-            privateKey: privateKey,
-            publicAddress: account.address.toLowerCase(),
-            testDuration: parseInt(duration),
-            multibaseKey,
-            env: {},
-          };
-          if (!skipNode) {
-            testEnv.env = {
-              SP_HTTP_ADDR: `127.0.0.1:${randomPort()}`,
-              SP_HTTP_INTERNAL_ADDR: `127.0.0.1:${randomPort()}`,
-              SP_DATA_DIR: tmpDir,
+            let testEnv: TestEnv = {
+              addr: "http://127.0.0.1:38080",
+              internalAddr: "http://127.0.0.1:39090",
+              privateKey: privateKey,
+              publicAddress: account.address.toLowerCase(),
+              testDuration: parseInt(duration),
+              multibaseKey,
+              env: {},
             };
+            if (!skipNode) {
+              testEnv.env = {
+                SP_HTTP_ADDR: `127.0.0.1:${randomPort()}`,
+                SP_HTTP_INTERNAL_ADDR: `127.0.0.1:${randomPort()}`,
+                SP_DATA_DIR: tmpDir,
+              };
+            }
+            if (test.setup) {
+              testEnv = await test.setup(testEnv);
+            }
+            if (!skipNode) {
+              const { addr, internalAddr, proc } = await makeNode({
+                env: testEnv.env,
+                autoQuit: false,
+              });
+              testEnv.addr = addr;
+              testEnv.internalAddr = internalAddr;
+              testProc = proc;
+            }
+            const result = await test.test(testEnv);
+            results.push(result);
+          } catch (e) {
+            console.error("error running test", e.message);
+            results.push(e.message);
+          } finally {
+            if (testProc) {
+              testProc.kill("SIGTERM");
+            }
           }
-          if (test.setup) {
-            testEnv = await test.setup(testEnv);
-          }
-          if (!skipNode) {
-            const { addr, internalAddr, proc } = await makeNode({
-              env: testEnv.env,
-              autoQuit: false,
-            });
-            testEnv.addr = addr;
-            testEnv.internalAddr = internalAddr;
-            testProc = proc;
-          }
-          const result = await test.test(testEnv);
-          results.push(result);
-        } catch (e) {
-          console.error("error running test", e.message);
-          results.push(e.message);
-        } finally {
-          if (testProc) {
-            testProc.kill("SIGTERM");
-          }
-        }
+        };
       })(test);
-      proms.push(prom);
+      funcs.push(testFunc);
     }
-    await Promise.all(proms);
+    if (series) {
+      for (const func of funcs) {
+        await func();
+      }
+    } else {
+      await Promise.all(funcs.map((f) => f()));
+    }
     const failures = results.filter((r) => r !== null);
     if (failures.length > 0) {
       console.error("tests failed", failures.join(", "));
