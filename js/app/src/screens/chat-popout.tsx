ca125bbf09df9055cbf9b457c07f690ebfe8b673
Natalie B. <22222885+espeon@users.noreply.github.com>
2025-07-31 21:44:01 -0500

    move popout chat to new chat

--- js/app/src/screens/chat-popout.tsx
+++ js/app/src/screens/chat-popout.tsx
@@ -1,6 +1,11 @@
-import { LivestreamProvider } from "@streamplace/components";
-import Chat from "components/chat/chat";
-import ChatBox from "components/chat/chat-box";
+import {
+  Chat,
+  ChatBox,
+  LivestreamProvider,
+  PlayerProvider,
+  ThemeProvider,
+} from "@streamplace/components";
+import emojiData from "assets/emoji-data.json";
 import { selectUserProfile } from "features/bluesky/blueskySlice";
 import { useAppSelector } from "store/hooks";
 import { View } from "tamagui";
@@ -12,19 +17,23 @@ export default function PopoutChat({ route }) {
   }
   const profile = useAppSelector(selectUserProfile);
   return (
-    <LivestreamProvider src={user}>
-      <View position="relative" f={1}>
-        <View
-          f={1}
-          position="absolute"
-          width="100%"
-          minHeight="100%"
-          bottom={0}
-        >
-          <Chat isChatVisible={true} setIsChatVisible={() => {}} />
-          {profile && <ChatBox isPopout={true} />}
-        </View>
-      </View>
-    </LivestreamProvider>
+    <ThemeProvider>
+      <LivestreamProvider src={user}>
+        <PlayerProvider>
+          <View position="relative" f={1} margin="$2">
+            <View
+              f={1}
+              position="absolute"
+              width="100%"
+              minHeight="100%"
+              bottom={0}
+            >
+              <Chat />
+              {profile && <ChatBox emojiData={emojiData} isPopout={true} />}
+            </View>
+          </View>
+        </PlayerProvider>
+      </LivestreamProvider>
+    </ThemeProvider>
   );
 }
