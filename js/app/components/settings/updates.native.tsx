7443eb92a291470ddf8f0525dda6ad8d457e54e8
Natalie Bridgers <natalie@iori.local>
2025-09-23 19:52:56 -0500

    try convert to zero-based styles for consistency

--- js/app/components/settings/updates.native.tsx
+++ js/app/components/settings/updates.native.tsx
@@ -1,4 +1,10 @@
-import { Text, useToast } from "@streamplace/components";
+import {
+  Button,
+  Text,
+  useTheme,
+  useToast,
+  zero,
+} from "@streamplace/components";
 import * as ExpoUpdates from "expo-updates";
 import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
@@ -6,6 +12,7 @@ import { Platform, TouchableOpacity, View } from "react-native";
 import pkg from "../../package.json";
 
 export function Updates() {
+  const theme = useTheme();
   const version = pkg.version;
   const updateInfo = ExpoUpdates.useUpdates();
   const { currentlyRunning, isUpdateAvailable, isUpdatePending } = updateInfo;
@@ -38,41 +45,63 @@ export function Updates() {
   return (
     <View
       style={[
-        { flex: 1 },
-        { alignItems: "center" },
-        { justifyContent: "center" },
-        { flexBasis: 0 },
+        zero.layout.flex.align.center,
+        zero.layout.flex.justify.center,
+        zero.gap.all[4],
       ]}
     >
-      <View>
-        <Text
-          style={[
-            {
-              fontSize: 24,
-              fontWeight: "bold",
-              textAlign: "center",
-              color: "#fff",
-            },
-          ]}
-        >
+      <View style={[zero.gap.all[2], zero.layout.flex.align.center]}>
+        <Text size="2xl" center style={[{ fontWeight: "bold", color: "#fff" }]}>
           Streamplace v{version}
         </Text>
-        <Text
+        <View
           style={[
-            {
-              fontSize: 18,
-              fontWeight: "600",
-              textAlign: "center",
-              paddingBottom: 20,
-              color: "#fff",
-            },
+            theme.zero.bg.muted,
+            zero.w.percent[100],
+            zero.px[2],
+            { borderRadius: 999 },
           ]}
         >
-          {runTypeMessage}
-        </Text>
+          <Text size="base" center>
+            {runTypeMessage}
+          </Text>
+        </View>
+      </View>
+      <Button
+        onPress={async () => {
+          try {
+            setChecked(true);
+            const res = await ExpoUpdates.checkForUpdateAsync();
+            if (!res.isAvailable) {
+              toast.show(
+                t("modal-latest-version"),
+                t("modal-no-update-available"),
+                { duration: 2000 },
+              );
+            } else {
+              toast.show(
+                t("modal-update-available-title"),
+                t("modal-update-available-description"),
+                { duration: 2000 },
+              );
+            }
+          } catch (e) {
+            toast.show(
+              t("modal-update-failed-title"),
+              t("modal-update-failed-description", {
+                store: Platform.OS === "ios" ? "App Store" : "Play Store",
+              }),
+            );
+          }
+        }}
+      >
+        <Text style={[{ color: "#fff", fontWeight: "600" }]}>{buttonText}</Text>
+      </Button>
+      {isUpdatePending && (
         <TouchableOpacity
           style={[
             {
+              marginTop: 8,
               backgroundColor: "#007AFF",
               borderRadius: 8,
               paddingHorizontal: 16,
@@ -80,59 +109,15 @@ export function Updates() {
               alignItems: "center",
             },
           ]}
-          onPress={async () => {
-            try {
-              setChecked(true);
-              const res = await ExpoUpdates.checkForUpdateAsync();
-              if (!res.isAvailable) {
-                toast.show(
-                  t("modal-latest-version"),
-                  t("modal-no-update-available"),
-                  { duration: 2000 },
-                );
-              } else {
-                toast.show(
-                  t("modal-update-available-title"),
-                  t("modal-update-available-description"),
-                  { duration: 2000 },
-                );
-              }
-            } catch (e) {
-              toast.show(
-                t("modal-update-failed-title"),
-                t("modal-update-failed-description", {
-                  store: Platform.OS === "ios" ? "App Store" : "Play Store",
-                }),
-              );
-            }
+          onPress={() => {
+            ExpoUpdates.reloadAsync();
           }}
         >
           <Text style={[{ color: "#fff", fontWeight: "600" }]}>
-            {buttonText}
+            {t("button-reload-app-on-update")}
           </Text>
         </TouchableOpacity>
-        {isUpdatePending && (
-          <TouchableOpacity
-            style={[
-              {
-                marginTop: 8,
-                backgroundColor: "#007AFF",
-                borderRadius: 8,
-                paddingHorizontal: 16,
-                paddingVertical: 12,
-                alignItems: "center",
-              },
-            ]}
-            onPress={() => {
-              ExpoUpdates.reloadAsync();
-            }}
-          >
-            <Text style={[{ color: "#fff", fontWeight: "600" }]}>
-              {t("button-reload-app-on-update")}
-            </Text>
-          </TouchableOpacity>
-        )}
-      </View>
+      )}
     </View>
   );
 }
