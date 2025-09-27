f8af675d9e06b3e4d183cf489302675489fa333b
Barry Carlyon <barry@barrycarlyon.co.uk>
2025-09-27 07:14:21 +0100

    Remove the slew of padding in this box Change the server and stream key display to readonly inputs Add an explict copy to clipboard button Change streamkey field to password and show it/select all on click

--- js/app/components/live-dashboard/stream-key.tsx
+++ js/app/components/live-dashboard/stream-key.tsx
@@ -2,17 +2,18 @@ import { Body, Button, Code, Row, View } from "@streamplace/components";
 import { Redirect } from "components/aqlink";
 import Loading from "components/loading/loading";
 import {
-  clearStreamKeyRecord,
-  createStreamKeyRecord,
-  selectIsReady,
-  selectUserProfile,
+    clearStreamKeyRecord,
+    createStreamKeyRecord,
+    selectIsReady,
+    selectUserProfile,
 } from "features/bluesky/blueskySlice";
 import { useEffect, useState } from "react";
+import { TextInput } from "react-native";
 import { useAppDispatch, useAppSelector } from "store/hooks";
 
 const FormRow = ({ children }: { children: React.ReactNode }) => {
   return (
-    <Row fullWidth padding="lg" align="start">
+    <Row fullWidth align="start">
       {children}
     </Row>
   );
@@ -66,18 +67,16 @@ export function StreamKeyScreen() {
             WHIP
           </Button>
         </FormRow>
-
         {protocol === "whip" && <WHIPDescription url={url} />}
         {protocol === "rtmp" && <RTMPDescription url={url} />}
-
         <FormRow>
           <Label>Output Settings</Label>
           <Content>
-            <Body>Output mode: Advanced</Body>
             <Body>
+              Output mode: Advanced
+              <br />
               Keyframe Interval: <Code>1s</Code>
-            </Body>
-            <Body>
+              <br />
               x264 Options: <Code>bframes=0</Code>
             </Body>
           </Content>
@@ -99,7 +98,20 @@ export function WHIPDescription({ url }: { url: string }) {
       <FormRow>
         <Label>Server</Label>
         <Content>
-          <Body>{url}</Body>
+          <TextInput
+            value={url}
+            readOnly={true}
+            style={[
+              {
+                backgroundColor: "#1a1a1a",
+                borderWidth: 1,
+                borderColor: "#333",
+                borderRadius: 8,
+                padding: 12,
+                color: "white",
+              },
+            ]}
+          />
         </Content>
       </FormRow>
       <FormRow>
@@ -127,7 +139,20 @@ export function RTMPDescription({ url }: { url: string }) {
       <FormRow>
         <Label>Server</Label>
         <Content>
-          <Body>{rtmpUrl}</Body>
+          <TextInput
+            value={rtmpUrl}
+            readOnly={true}
+            style={[
+              {
+                backgroundColor: "#1a1a1a",
+                borderWidth: 1,
+                borderColor: "#333",
+                borderRadius: 8,
+                padding: 12,
+                color: "white",
+              },
+            ]}
+          />
         </Content>
       </FormRow>
       <FormRow>
@@ -145,6 +170,8 @@ export default StreamKeyScreen;
 export function StreamKey() {
   const dispatch = useAppDispatch();
   const [generating, setGenerating] = useState(false);
+  const [hidekey, setHidekey] = useState(true);
+  const [didcopy, setDidcopy] = useState(false);
   const newKey = useAppSelector((state) => state.bluesky.newKey);
 
   useEffect(() => {
@@ -168,12 +195,95 @@ export function StreamKey() {
     };
   }, [newKey, dispatch]);
 
+  const handleCopy = async () => {
+    if (!newKey) {
+      return;
+    }
+
+    try {
+      await navigator.clipboard.writeText(newKey.privateKey);
+      // TODO: Replace with custom toast implementation
+      console.log("Bearer token copied to clipboard");
+      setDidcopy(true);
+    } catch (e) {
+      // not allowed. oh well.
+      console.log("Could not copy to clipboard");
+    }
+  };
+
   if (generating) {
     return <Loading />;
   }
 
   if (newKey) {
-    return <Code>{newKey.privateKey}</Code>;
+
+    return (
+      <Row
+        fullWidth
+        flex={1}
+        align="start"
+      >
+        <TextInput
+          value={newKey.privateKey}
+          secureTextEntry={hidekey}
+          readOnly={true}
+          style={[
+            {
+              backgroundColor: "#1a1a1a",
+              borderWidth: 1,
+              borderColor: "#333",
+              borderRadius: 8,
+              padding: 12,
+              color: "white",
+              flex: 1,
+              borderTopRightRadius: "0px",
+              borderBottomRightRadius: "0px",
+            },
+          ]}
+          onFocus={(e) => {
+            setHidekey(false);
+          }}
+          onBlur={() => {
+            setHidekey(true);
+          }}
+          selectTextOnFocus={true}
+        />
+        <Button
+          onPress={handleCopy}
+          style={[
+            {
+              borderTopLeftRadius: "0px",
+              borderBottomLeftRadius: "0px",
+            },
+          ]}
+        >
+          {didcopy ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              viewBox="0 0 16 16"
+            >
+              <path d="M9.5 0a.5.5 0 0 1 .5.5.5.5 0 0 0 .5.5.5.5 0 0 1 .5.5V2a.5.5 0 0 1-.5.5h-5A.5.5 0 0 1 5 2v-.5a.5.5 0 0 1 .5-.5.5.5 0 0 0 .5-.5.5.5 0 0 1 .5-.5z" />
+              <path d="M3 2.5a.5.5 0 0 1 .5-.5H4a.5.5 0 0 0 0-1h-.5A1.5 1.5 0 0 0 2 2.5v12A1.5 1.5 0 0 0 3.5 16h9a1.5 1.5 0 0 0 1.5-1.5v-12A1.5 1.5 0 0 0 12.5 1H12a.5.5 0 0 0 0 1h.5a.5.5 0 0 1 .5.5v12a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5z" />
+              <path d="M10.854 7.854a.5.5 0 0 0-.708-.708L7.5 9.793 6.354 8.646a.5.5 0 1 0-.708.708l1.5 1.5a.5.5 0 0 0 .708 0z" />
+            </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              viewBox="0 0 16 16"
+            >
+              <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1z" />
+              <path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0z" />
+            </svg>
+          )}
+        </Button>
+      </Row>
+    );
   }
 
   return (
@@ -181,6 +291,7 @@ export function StreamKey() {
       onPress={async () => {
         try {
           setGenerating(true);
+          setDidcopy(false);
           await dispatch(createStreamKeyRecord({ store: false }));
         } catch (e) {
           console.error("failed to generate stream key", e);
