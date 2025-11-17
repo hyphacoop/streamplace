1a6a3d88a5e4fc160e4003275083803b4fce6195
Eli Mallon <eli@iame.li>
2025-11-16 19:43:49 -0800

    tests: move stuck segment to a failure case

--- pkg/media/segment_converge_test.go
+++ pkg/media/segment_converge_test.go
@@ -12,22 +12,26 @@ import (
 )
 
 type ConvergeCase struct {
-	Name string
-	File string
+	Name    string
+	File    string
+	Success bool
 }
 
 var ConvergeCases = []ConvergeCase{
 	{
-		Name: "Good",
-		File: remote.RemoteFixture("dbce5682132f9f1a8d92e1dcd66da99e4ae6eefd7429e4b168ed05d721a80379/2025-11-14T21-10-51-750Z-attempt-000.mp4"),
+		Name:    "Good",
+		File:    remote.RemoteFixture("dbce5682132f9f1a8d92e1dcd66da99e4ae6eefd7429e4b168ed05d721a80379/2025-11-14T21-10-51-750Z-attempt-000.mp4"),
+		Success: true,
 	},
 	{
-		Name: "Evil",
-		File: remote.RemoteFixture("d81395168f8b2f3361d8e6d3443eeb678285a1973dc0b31e966cb81f5916db48/2025-11-14T21-10-57-754Z-attempt-000.mp4"),
+		Name:    "Evil",
+		File:    remote.RemoteFixture("d81395168f8b2f3361d8e6d3443eeb678285a1973dc0b31e966cb81f5916db48/2025-11-14T21-10-57-754Z-attempt-000.mp4"),
+		Success: true,
 	},
 	{
-		Name: "Stuck",
-		File: remote.RemoteFixture("77e32825eaa9dfb8f6c7bbe3cb21213ffa01c1dc0d041f8e3e9cc4d107c95f16/2025-11-17T01-08-56-070Z-converge-segment-did-key-zQ3shX7nQpEqXEp3XFSPkS7mtUjQ3S1MNvxrEP2HeiwyPqmoz.mp4"),
+		Name:    "Stuck",
+		File:    remote.RemoteFixture("77e32825eaa9dfb8f6c7bbe3cb21213ffa01c1dc0d041f8e3e9cc4d107c95f16/2025-11-17T01-08-56-070Z-converge-segment-did-key-zQ3shX7nQpEqXEp3XFSPkS7mtUjQ3S1MNvxrEP2HeiwyPqmoz.mp4"),
+		Success: false,
 	},
 	// {
 	// 	Name: "CrashedPipeline",
@@ -46,8 +50,12 @@ func TestConvergeSegment(t *testing.T) {
 				bs, err := os.ReadFile(tc.File)
 				require.NoError(t, err)
 				bs, err = ConvergeSegment(ctx, &config.CLI{}, bs, 0, "test-streamer")
-				require.NoError(t, err)
-				require.Greater(t, len(bs), 0)
+				if tc.Success {
+					require.NoError(t, err)
+					require.Greater(t, len(bs), 0)
+				} else {
+					require.Error(t, err)
+				}
 			})
 		})
 	}
